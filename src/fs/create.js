5bce9a29aeacf5a1f31eeccbe99c87ea2e894884
Evgeniya Burlachenko <63720882+evgeniya-burlachenko@users.noreply.github.com>
2022-05-31 13:20:50 +0300

    Update create.js

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,3 +1,9 @@
 export const create = async () => {
-    // Write your code here 
+    let fs = require('fs');
+    if (!fs.existsSync('fresh.txt')){
+	    fs.writeFileSync('fresh.txt','I am fresh and young', function(error){
+   		    if(error) throw error;})
+        } else {
+	        console.error('FS operation failed');
+        }
 };
