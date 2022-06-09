70e4dc1b3e6928f9e69351ce8277976d511c20b5
Evgeniya Burlachenko <63720882+evgeniya-burlachenko@users.noreply.github.com>
2022-06-09 16:22:27 +0300

    Update create.js
    fix: fixed the bug

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,9 +1,27 @@
+import { fileURLToPath } from 'url'
+import { dirname } from 'path'
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
+import fs from 'fs'
+//синхронно:
+// export const create = async () => {
+//     if (!fs.existsSync('fresh.txt')){
+// 	    fs.writeFileSync('fresh.txt', 'I am fresh and young', function(error){
+//    		    if(error) throw error;})
+//         }else {
+// 	        console.error('FS operation failed');
+//         }
+// };
+//асинхронно:
 export const create = async () => {
-    let fs = require('fs');
-    if (!fs.existsSync('fresh.txt')){
-	    fs.writeFileSync('fresh.txt','I am fresh and young', function(error){
-   		    if(error) throw error;})
-        } else {
-	        console.error('FS operation failed');
-        }
+	fs.access('fresh.txt', fs.F_OK, (err) => {
+		if (err) {
+			fs.writeFile('fresh.txt','I am fresh and young', function(error){
+				if(error) throw error;})
+		  return;
+		}else{
+			console.log(new Error('FS operation failed'));
+		}
+	})	
 };
+create();
